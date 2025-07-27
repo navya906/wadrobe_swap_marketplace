b410b08a80c8480c13979acd280e5717a9e2ef88
Ekanshii <ekanshi.yadav2024@vitstudent.ac.in>
2025-07-28 01:38:37 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import SwipePage from './components/Swipe/SwipePage';
+import SwipePage from './components/swap/SwapPage';
 import ProfilePage from './components/Profile/ProfilePage';
 import ChatPage from './components/Chat/ChatPage';
+
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Route path="/" element={<Navigate to="/swap" />} />
       <Route path="/swap" element={<SwipePage />} />
       <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/chat/:userId" element={<ChatPage />} />
+
     </Routes>
   );
 }
--- src/components/swap/ClothingCard.jsx
+++ src/components/swap/ClothingCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './ClothingCard.css';
 
 const ClothingCard = ({ item, onSwipe }) => {
   return (
-    <div style={{ border: '1px solid #ccc', padding: 10, marginBottom: 20 }}>
-      <img src={item.image} alt={item.name} width="300" height="400" />
+    <div className="card">
+      <img src={item.image} alt={item.name} />
       <h3>{item.name}</h3>
       <button onClick={() => onSwipe('left', item)}>❌ Left</button>
       <button onClick={() => onSwipe('right', item)}>✅ Right</button>
